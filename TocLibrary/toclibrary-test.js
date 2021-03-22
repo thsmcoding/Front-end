cb523e0a492ad157ce52f80bb176e157ace5f3b0
thsmcoding <hassia.tamimou@gmail.com>
2021-03-22 23:32:48 +0100

    FIX: fixing function getHeadingNumber. Manual testing passed.

---
+++ TocLibrary/toclibrary-test.js
@@ -0,0 +1,5 @@
+QUnit.test("Testing function SetUpToc", function( assert ) {
+	var content = document.getElementById("toc_list");
+	var unexpected = null;
+	assert.notEqual(content, unexpected, "The answer should be TRUE' ");
+});
